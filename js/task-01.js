6a9f61c928c2e3b7f510f80d3760e3bb0bcfbba9
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-02 00:19:37 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,12 +5,21 @@ console.log('Number of categories:', listEl.length);
 
 
 const titleEl = document.querySelectorAll('h2');
+
+
+
+const allListEl = document.querySelectorAll('ul')
+
+const firstItemEl = allListEl[1].children;
+const secondItemEl = allListEl[2].children;
+const thirdItemEl = allListEl[3].children;
+
+
 console.log('Category:', titleEl[0].textContent);
+console.log('Elements:',firstItemEl.length);
 
 console.log('Category:', titleEl[1].textContent);
+console.log('Elements:',secondItemEl.length);
 
 console.log('Category:', titleEl[2].textContent);
-
-
-const itemEl = document.querySelectorAll('li')
-console.log(itemEl)
+console.log('Elements:',thirdItemEl.length);
