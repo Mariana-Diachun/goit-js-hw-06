18f01acb692ad719c279be21ba888b3e58932b9a
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-02 23:31:38 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,32 +7,16 @@ const ingredients = [
   'Condiments',
 ];
 
-const firstItemEl = document.createElement("li");
-firstItemEl.textContent = ingredients[0]
-firstItemEl.classList.add("item")
+const ingredientContainer = document.querySelector('#ingredients');
+console.log(ingredientContainer);
 
 
-const secondItemEl = document.createElement("li");
-secondItemEl.textContent = ingredients[1]
-secondItemEl.classList.add("item")
+const itemElements = ingredients.map(index => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = index;
+  return itemEl;
+})
+console.log(itemElements);
 
-const thirdItemEl = document.createElement("li");
-thirdItemEl.textContent = ingredients[2]
-thirdItemEl.classList.add("item")
-
-const fouthItemEl = document.createElement("li");
-fouthItemEl.textContent = ingredients[3]
-fouthItemEl.classList.add("item")
-
-const fifthItemEl = document.createElement("li");
-fifthItemEl.textContent = ingredients[4]
-fifthItemEl.classList.add("item")
-
-const sixthItemEl = document.createElement("li");
-sixthItemEl.textContent = ingredients[5]
-sixthItemEl.classList.add("item")
-
-
-const listEl = document.querySelector("#ingredients");
-listEl.append(firstItemEl, secondItemEl, thirdItemEl, fouthItemEl, fifthItemEl, sixthItemEl);
-console.log(listEl)
+ingredientContainer.append(...itemElements);
