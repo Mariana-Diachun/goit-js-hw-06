4a3c8020181205e9d9a48f155d8ce8e363a1f405
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-05 00:08:51 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#validation-input');
+console.log(inputEl);
+
+
+inputEl.addEventListener
+
+
+
+inputEl.addEventListener("blur", () => {
+console.log(inputEl.value = "");
+});
