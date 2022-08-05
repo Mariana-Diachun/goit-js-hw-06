c83336997706fd509e318251e6c4e8d6f831caea
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-05 17:16:59 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,13 @@
 const inputEl = document.querySelector('#validation-input');
 console.log(inputEl);
 
-
-inputEl.addEventListener
-
-
-
+// console.log(typeof Number(inputEl.dataset.length))
+// console.log(inputEl.value.length)
 inputEl.addEventListener("blur", () => {
-console.log(inputEl.value = "");
+    if (inputEl.value.length === Number(inputEl.dataset.length)) {
+        return inputEl.classList.add('valid');
+    } else
+        return inputEl.classList.replace('valid', 'invalid');
 });
+
+
