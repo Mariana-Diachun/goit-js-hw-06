21b6077ba24004fd47382facaa1a38c654201702
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-05 18:17:32 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return console.log("Alert!!! Please fill in all the fields!");
+    } else
+        return console.log(`Email: ${email.value}, Password: ${password.value}`);
+        event.currentTarget.reset()
+})
