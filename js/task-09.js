e37068eafed4b01cfc5d609adfb2b0878268cbce
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-05 22:25:50 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+console.log(buttonEl);
+
+const spanEl = document.querySelector('.color');
+console.log(spanEl);
+
+const body = document.querySelector('body');
+console.log(body);
+
+buttonEl.addEventListener('click', onButtonEl);
+
+function onButtonEl(event) {
+  spanEl.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
